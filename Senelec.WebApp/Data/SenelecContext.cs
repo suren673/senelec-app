f96a343a6d7392a429a0caafa908935b7f66f344
Houssem Dellai <hodellai@microsoft.com>
2020-02-18 16:08:02 +0100

    Add project files.

---
+++ Senelec.WebApp/Data/SenelecContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Senelec.WebApp.Models;
+
+namespace Senelec.WebApp.Data
+{
+    public class SenelecContext : DbContext
+    {
+        public SenelecContext (DbContextOptions<SenelecContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Senelec.WebApp.Models.Product> Product { get; set; }
+    }
+}
